8f2d3cb4b34a1507e03062bc2cde0fc10eef51f5
okhobian <76004407+okhobian@users.noreply.github.com>
2022-09-27 11:35:08 -0500

    compose train test

--- learning/data2.py
+++ learning/data2.py
@@ -5,6 +5,10 @@ from numpy import argmax
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
 
 
 class ADLNORMAL:
@@ -33,7 +37,7 @@ class ADLNORMAL:
             
             ## for all instances of the current activity
             for i, idx in enumerate( activity_idx ):
-                if i>3: break 
+                # if i>0: break 
                 sequence = self.df.loc[idx[0]:idx[1]]
                 sensor_sequence = sequence.loc[:,'Sensor'].to_numpy()
                 sequences.append(np.array(sensor_sequence))
@@ -46,13 +50,47 @@ class ADLNORMAL:
         return np.array(sequences), np.array(labels)
     
     def compose_train_test_sets(self):
-        s, l = self.extract_sequences(self.activities)
         
-        TODO: 
-            # 1. onehot s and l;
-            # 2. spilit into train, test
+        # 1. onehot s and l;
+        s, l = self.extract_sequences()
+        
+        unique_col_vals = self.df['Sensor'].dropna().unique()   # extract unique sensor types from data
+        values = sorted( array(unique_col_vals) )               # sort alphabetically unique sensor types
+        word_idx = {k: v+1 for v, k in enumerate(values)}       # reserve 0 for padding
+                
+        tokenizer = Tokenizer(filters="")                       # empty filter to avoid spliting sensor name with '_'
+        tokenizer.word_index = word_idx                         # initialize word index
+        
+        ## tokenize sequences
+        tokenized_seq = []
+        for seq in s:
+            tokenizer.fit_on_texts(seq)
+            sequences = tokenizer.texts_to_sequences(seq) 
+            tokenized_seq.append(sequences)
+        
+        # one-hot encode labels
+        integer_mapping = {x: i for i,x in enumerate(l)}
+        vec = [integer_mapping[word] for word in l]
+        onehot_labels = to_categorical(vec, num_classes=len(vec))
+        # print(onehot_labels)
+        
+        ## example of getting back to real values
+        # indices = np.argmax(onehot_labels, axis=-1)
+        # print(indices)
+        # print(l[indices[0]])
+        
+        # print(len(tokenized_seq))
+        # print(len(onehot_labels))
+        
+        # 2. spilit into train, test    
+        X_train, X_test, y_train, y_test = train_test_split(tokenized_seq, onehot_labels,
+                                                            random_state=42, 
+                                                            test_size=0.2, 
+                                                            shuffle=True)
             
-        return None, None
+        return X_train, X_test, y_train, y_test
+
+        
 
 if __name__ == '__main__':
     
@@ -64,7 +102,7 @@ if __name__ == '__main__':
     
     data = ADLNORMAL()
     data.load_data(DATA_FILE, columns, activities)
-    train, test = data.compose_train_test_sets()
+    X_train, X_test, y_train, y_test = data.compose_train_test_sets()
     
     
     
