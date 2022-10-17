adc98be7e59e06a84e48211bb893e81318f1e505
okhobian <76004407+okhobian@users.noreply.github.com>
2022-10-17 14:16:03 -0500

    compare windowsize

---
+++ learning/inuse/lstm_compare.py
@@ -0,0 +1,34 @@
+from data import *
+from models import *
+from history_callback import *
+from plot import *
+
+## VARIABLES
+BASE_PATH = '/Users/hobian/Desktop/GitHub/lstm-situ'
+DATA_FILE = f'{BASE_PATH}/datasets/openshs/dataset.csv'
+ACTIVITIES = ['sleep', 'eat', 'personal', 'work', 'leisure', 'anomaly', 'other']
+WINDOW_SIZE = [5, 10, 15, 20]
+LABEL_AHEAD = 1
+
+## LOAD DATA
+data = DATASET()
+data.load_data(DATA_FILE, ACTIVITIES)
+histories = Histories()
+plt = PLOT()
+
+accuracies = []
+losses = []
+times = []
+for size in WINDOW_SIZE:
+    trainX, trainY = data.train_data(size, LABEL_AHEAD)
+    model = build_lstm(trainX.shape[1], trainX.shape[2], trainY.shape[1])
+    model.fit(trainX, trainY, batch_size=50, epochs=1, verbose=1, callbacks=[histories])  # validation_data=(x_test, y_test),
+    accuracies.append(histories.accuracies)
+    losses.append(histories.losses)
+    times.append(histories.times)
+
+
+plt.add_multi_data_figure(accuracies, 'Accuracies', 'batch#', 'accuracy', WINDOW_SIZE)
+plt.add_multi_data_figure(losses, 'Losses', 'batch#', 'loss', WINDOW_SIZE)
+plt.add_multi_data_figure(times, 'Training time', 'batch#', 'time', WINDOW_SIZE)
+plt.show_all()
--- learning/inuse/plot.py
+++ learning/inuse/plot.py
@@ -1,3 +1,4 @@
+from logging import raiseExceptions
 from matplotlib import pyplot as plt
 
 class PLOT():
@@ -12,27 +13,19 @@ class PLOT():
         self.plt.legend(legend, loc='upper left')
         self.plt.figure()
 
+    def add_multi_data_figure(self, multi_data, title, xlabel, ylabel, legend):
+        
+        if len(multi_data) != len(legend):
+            print("ERROR: len(legend) != len(data)")
+        
+        for data in multi_data:
+            self.plt.plot(data)
+            
+        self.plt.title(title)
+        self.plt.xlabel(xlabel)
+        self.plt.ylabel(ylabel)
+        self.plt.legend(legend, loc='upper left')
+        self.plt.figure()
+
     def show_all(self):
         self.plt.show()
-
-# plt.plot(histories.losses)
-# plt.title('model losses')
-# plt.ylabel('loss')
-# plt.xlabel('batch')
-# plt.legend(['loss'], loc='upper left')
-# plt.figure()
-
-# plt.plot(histories.accuracies)
-# plt.title('model accuracies')
-# plt.ylabel('accuracy')
-# plt.xlabel('batch')
-# plt.legend(['accuracy'], loc='upper left')
-# plt.figure()
-
-# plt.plot(histories.times)
-# plt.title('training time')
-# plt.ylabel('time')
-# plt.xlabel('batch')
-# plt.legend(['time'], loc='upper left')
-
-# plt.show()
