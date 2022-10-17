cd6986b7b23a3f36b196b45ada075538a4450aa0
okhobian <76004407+okhobian@users.noreply.github.com>
2022-10-17 13:13:10 -0500

    plot batch acc loss

--- learning/lstm3.py
+++ learning/lstm3.py
@@ -22,25 +22,53 @@ model.add(LSTM(64, activation='relu', input_shape=(trainX.shape[1], trainX.shape
 model.add(LSTM(32, activation='relu', return_sequences=False))
 model.add(Dropout(0.2))
 model.add(Dense(trainY.shape[1], activation='softmax'))
-# model.add(Dense(trainY.shape[1]))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model.compile(optimizer='adam', loss='mse')
 model.summary()
+# history = model.fit(trainX, trainY, epochs=5, batch_size=1, validation_split=0.3, verbose=1)
 
-history = model.fit(trainX, trainY, epochs=5, batch_size=1, validation_split=0.3, verbose=1)
+###################################################
+
+from keras.callbacks import Callback
+
+class Histories(Callback):
+
+    def on_train_begin(self,logs={}):
+        self.losses = []
+        self.accuracies = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.accuracies.append(logs.get('accuracy'))
 
 
-plt.plot(history.history['val_accuracy'])
-plt.plot(history.history['val_loss'])
-plt.title('model accuracy')
+histories = Histories()
+
+model.fit(trainX, trainY, batch_size=20, epochs=1, verbose=1,
+        #   validation_data=(x_test, y_test),
+          callbacks=[histories]
+         )
+
+
+plt.plot(histories.losses)
+plt.title('model losses')
+plt.ylabel('loss')
+plt.xlabel('batch')
+plt.legend(['loss'], loc='upper left')
+plt.figure()
+
+plt.plot(histories.accuracies)
+plt.title('model accuracies')
 plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['val_accuracy', 'val_loss'], loc='upper left')
-# plt.figure()
+plt.xlabel('batch')
+plt.legend(['accuracy'], loc='upper left')
+
+# plt.plot(histories.accuracies)
+# plt.plot(histories.losses)
+# plt.title('model accuracies & losses')
+# plt.ylabel('acc')
+# plt.xlabel('batch')
+# plt.legend(['acc', 'loss'], loc='upper left')
 
 
-# plt.plot(history.history['loss'], label='Training loss')
-# plt.plot(history.history['val_loss'], label='Validation loss')
-# plt.legend()
 plt.show()
